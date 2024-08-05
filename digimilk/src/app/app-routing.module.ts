94a91bd0e5865b16c6e61ff0f6079b0b64c32bb5
prernarawat77 <prenarawat@ourappshop.in>
2024-08-05 23:09:50 +0530

    solved the problem with the error and created login page

--- digimilk/src/app/app-routing.module.ts
+++ digimilk/src/app/app-routing.module.ts
@@ -46,11 +46,6 @@ const routes: Routes = [
   { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule)},
   { path: '**', redirectTo: 'dashboard', },
   { path: '**', component: PageNotFoundComponent },
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/prernarawat77
 ];
 
 @NgModule({
