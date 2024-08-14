dfc2d5c1a376e8de6a3446f5db3b4987600f0213
Milind Diwadkar <165246230+milinddiwadkar@users.noreply.github.com>
2024-08-14 17:52:53 +0530

    Menubar pop up on login page issue fixed

--- digimilk/src/app/app.component.ts
+++ digimilk/src/app/app.component.ts
@@ -1,40 +1,43 @@
 import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
 import { Router, NavigationEnd } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  dropdowns: { [key: string]: boolean } = {
+  public showMenuBar = true;  // Menu visibility flag
+  public isLoginPage = false; // Flag to check if it's the login page
+  public dropdowns: { [key: string]: boolean } = {
     users: false,
-    reports: false,
     masters: false,
     farmerDemand: false,
+    reports: false,
     broadcastMessage: false
   };
 
-  showMenuBar = false;
-
-
-
-  constructor(private menu: MenuController, private router: Router) {
+  constructor(private router: Router) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.showMenuBar = !event.url.includes('/login');
+        // Check if the current route is the login page
+        this.isLoginPage = this.router.url === '/login';
+        // Show or hide the menu based on the route
+        this.showMenuBar = !this.isLoginPage;
       }
     });
-  
-  }
-
-  toggleDropdown(key: string) {
-    this.dropdowns[key] = !this.dropdowns[key];
   }
 
   closeMenu() {
-    this.menu.close('main-menu');
+    const menu = document.querySelector('ion-menu');
+    if (menu) {
+      menu.close();
+    }
+  }
+
+  toggleDropdown(dropdown: string) {
+    if (this.dropdowns.hasOwnProperty(dropdown)) {
+      this.dropdowns[dropdown] = !this.dropdowns[dropdown];
+    }
   }
 }
