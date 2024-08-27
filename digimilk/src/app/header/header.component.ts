bb9f1bc9f08ef47b50244507710d752069e1d68d
prernarawat77 <prenarawat@ourappshop.in>
2024-08-27 18:15:56 +0530

    modify the menubar

--- digimilk/src/app/header/header.component.ts
+++ digimilk/src/app/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent  implements OnInit {
-  
+export class HeaderComponent {
+  searchQuery: string = '';
+  isUserMenuOpen: boolean = false;
+  notifications: number = 5;
 
-  constructor() { }
-  ngOnInit() {}
-  
+  constructor(private router: Router) {}
 
+  toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  closeUserMenu() {
+    this.isUserMenuOpen = false;
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+    this.closeUserMenu();
+  }
+
+  logout() {
+    // Implement logout logic
+    console.log('User logged out');
+  }
+
+  performSearch() {
+    // Implement search logic
+    console.log('Search query:', this.searchQuery);
+  }
 }
